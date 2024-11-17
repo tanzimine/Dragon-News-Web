6fec18dd06fea3ef2d0ab393f78661209f839d53
Tanzim vai <khandakertanzimmahtab@gmail.com>
2024-11-16 23:53:34 -0500

    added firebase  authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
 import { Link } from "react-router-dom";
 import userIcon from "../assets/user.png";
+import { AuthContext } from "../provider/AuthProvider";
+import { useContext } from "react";
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
   return (
     <div className="flex justify-between items-center">
-      <div className=""></div>
+      <div className="">{user && user.email}</div>
       <div className="nav space-x-5">
         <Link to="/">Home</Link>
         <Link to="/career">Career</Link>
         <Link to="/about">About</Link>
+        <Link to="/dev">Dev Information</Link>
       </div>
       <div className="login flex gap-2 items-center">
         <div className=" ">
-          <img src={userIcon} alt="" />
+          {user && user?.email ? (
+            <div>
+              <img className="w-10 rounded-full" src={user?.photoURL} alt="" />
+              <p>{user.displayName}</p>
+            </div>
+          ) : (
+            <img src={userIcon} alt="" />
+          )}
         </div>
-        <Link to="/auth/login" className="btn btn-neutral rounded-none">Login</Link>
+        {user && user?.email ? (
+          <button onClick={logOut} className="btn btn-neutral rounded-none">
+            Log-Out
+          </button>
+        ) : (
+          <Link to="/auth/login" className="btn btn-neutral rounded-none">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
+
+const Login = () => {
+  const { userLogin, setUser } = useContext(AuthContext);
+  const [error, setError] = useState({});
+  const location = useLocation();
+  const navigate = useNavigate();
+  // console.log(location);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log({ email, password });
+    userLogin(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((err) => {
+        setError({ ...error, login: err.code });
+      });
+  };
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-2xl font-semibold text-center">
+          Login your account
+        </h2>
+        <form onSubmit={handleSubmit} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            {/* -------------ERRRRRORR?????????---------------- */}
+            {error.login && (
+              <label className="label text-sm text-red-600">
+                {error.login}
+              </label>
+            )}
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none">Login</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">
+          Dont’t Have An Account ?{" "}
+          <Link className="text-red-500" to="/auth/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
